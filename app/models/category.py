747da9d80b42b45f283dbbd36d95162e54e703a4
Matheus A <maf_dev@hotmail.com>
2026-02-17 20:07:09 -0300

    add models for products and categories ready for initial ORM

--- app/models/category.py
+++ app/models/category.py
@@ -1,2 +1,28 @@
-class Category:
-    ...
+from app.models.base import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, relationship, mapped_column
+
+class Category(Base):
+    __tablename__ = "categories"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    products: Mapped[list["Product"]] = relationship(back_populates="category") # type: ignore
+    
+    @classmethod
+    def from_dict(cls, data:dict):        
+        if not isinstance(data, dict):
+            raise TypeError(f"Data must be a dict. Got {type(data).__name__}")
+
+        if not data:
+            raise ValueError(f"Empty dictionary provided")
+        
+        id = data.get("id", None)
+        name = data.get("name", None)
+        
+        if not id or not name:
+            raise ValueError (f"Missing required fields: 'id' and 'name' are required")
+        
+        return cls(id=id, name=name)
+    
+    def __repr__(self) -> str:
+        return f"Category(id={self.id!r}, name={self.name!r}"
