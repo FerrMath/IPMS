8a1bfcabe5160587a7d4f81dc7a9ab4668439a77
Matheus A <maf_dev@hotmail.com>
2026-02-28 13:40:22 -0300

    feat: implement generic persistence controller for models

---
+++ app/controllers/general.py
@@ -0,0 +1,57 @@
+from sqlalchemy.orm import Session, DeclarativeBase
+from sqlalchemy import select
+from typing import Type, Generic, TypeVar
+from functools import wraps
+from app.db.engine import SessionLocal
+
+T = TypeVar("T", bound=DeclarativeBase)
+
+def transactional(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        with SessionLocal() as session:
+            try:
+                result = func(*args, session=session, **kwargs)
+                session.commit()
+                return result
+            except:
+                session.rollback()
+                raise
+    return wrapper
+
+class GeneralController(Generic[T]):
+    def __init__(self, model: Type[T]):
+        self.model = model
+    
+    def save(self, session: Session, obj:T) -> T:
+        session.add(obj)
+        session.flush()
+        session.refresh(obj)
+        return obj
+    
+    def get_all(self, session: Session) -> list[T]:
+        stmt = select(self.model)
+        result = session.scalars(stmt).all()
+        return list(result)
+    
+    def get_by_id(self, session:Session, obj_id:int) -> T | None:
+        return session.get(self.model, obj_id)
+    
+    def update(self, session: Session, obj_id:int, data: dict) -> T | None:
+        obj = session.get(self.model, obj_id)
+        
+        if not obj:
+            return None
+        
+        for k, v in data.items():
+            if hasattr(obj, k):
+                setattr(obj, k, v)
+            else:
+                raise ValueError(f"invalid field []")
+            
+        session.flush()
+        session.refresh(obj)
+        return obj
+    
+    def delete(self, session:Session, obj: T) -> None:
+        session.delete(obj)
